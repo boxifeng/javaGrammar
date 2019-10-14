589152947887b21281a560d6d871840958a808d3
薄熙峰 <727178669@qq.com>
2019-10-15 01:04:30 +0800

    arrayTest

--- array/src/com/becc/arraydemo/ArrayDemo01.java
+++ array/src/com/becc/arraydemo/ArrayDemo01.java
@@ -3,5 +3,7 @@ package com.becc.arraydemo;
 public class ArrayDemo01 {
     public static void main(String[] args) {
         System.out.println("arrayTest");
+
+        System.out.println("sdfsdfsfd");
     }
 }
