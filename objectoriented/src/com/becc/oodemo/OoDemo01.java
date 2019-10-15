0cd3ed872b1a277b03807c0aeeb76def81efeca3
薄熙峰 <727178669@qq.com>
2019-10-15 23:55:30 +0800

    面向对象

--- objectoriented/src/com/becc/oodemo/OoDemo01.java
+++ objectoriented/src/com/becc/oodemo/OoDemo01.java
@@ -1,4 +1,11 @@
 package com.becc.oodemo;
 
+import java.util.Arrays;
+
 public class OoDemo01 {
+    public static void main(String[] args) {
+        int[] ints = {10, 20, 30, 40, 50, 60};
+        System.out.println(Arrays.toString(ints));
+
+    }
 }
