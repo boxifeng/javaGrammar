0340b841b249bb51d517be31d6150fe542d04e35
727178669@qq.com <cfah4342173>
2019-10-16 15:14:50 +0800

    写一种自己思路的反转数组方法 实现对所有引用类型数组的反转

--- array/src/com/becc/arraydemo/ArrayDemo05.java
+++ array/src/com/becc/arraydemo/ArrayDemo05.java
@@ -1,18 +1,36 @@
 package com.becc.arraydemo;
 
+import java.util.Arrays;
+
 /**
- *反转一个数组(任意类型)
+ *反转一个数组(任意引用类型)
  */
 public class ArrayDemo05 {
+    /**
+     * 测试方法
+     * @param args
+     */
     public static void main(String[] args) {
-        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 44, 66, 777, 888, 999};
-        Object[] objects = reverseArray(array);
+        Integer[] array = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 44, 66, 777, 888, 999};
+        String[] StringArray = new String []{"1111","2222","333","4444","666"};
+        Integer[] integers = reverseArray(array);
+        String[] strings = reverseArray(StringArray);
+        System.out.println(Arrays.toString(integers));
+        System.out.println(Arrays.toString(strings));
+
     }
-    public static <T>T reverseArray(T[] array){
-        Object[] objectArray = new Object[array.length];
+
+    /**
+     * 反转方法
+     * @param array
+     * @param <T>
+     * @return
+     */
+    public static <T>T[] reverseArray(T[] array){
+        T[] objectArray = array.clone();
         for (int i = 0; i < array.length; i++) {
             objectArray[array.length - (i+1)] = array[i];
         }
-        return (T) objectArray;
+        return objectArray;
     }
 }
