660a83bb6ef706ff2467f00120712f0179e35e97
薄熙峰 <727178669@qq.com>
2019-10-21 01:24:30 +0800

    求两个键盘输入int类型的和的方法

---
+++ api/src/Demo01Scanner.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * 键盘输入两个数字，输出和值
+ */
+public class Demo01Scanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sum(sc);
+    }
+
+    /**
+     * 求两个int类型的和
+     * @param sc
+     */
+    public static void sum(Scanner sc) {
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        int k = i + j;
+        System.out.println("输出的和为" + k);
+    }
+}
