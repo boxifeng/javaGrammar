2366141d9275b13d8b851556b9c99d64c7a8a293
薄熙峰 <727178669@qq.com>
2019-10-21 01:35:24 +0800

    2019年10月21日01:35:12

---
+++ api/src/Demo02Scanner.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Demo02Scanner {
+    public static void main(String[] args) {
+        System.out.println(getMaxInt());
+    }
+
+    /**
+     * 返回键盘输入的三个int中最大的数字
+     * @return
+     */
+    public static int getMaxInt() {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        int k = sc.nextInt();
+        int temp = (i <= j) ? j : i;
+        temp = (temp <= k) ? k : temp;
+        return temp;
+    }
+}
