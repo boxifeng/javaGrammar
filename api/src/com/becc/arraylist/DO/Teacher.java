5097c3e9b506db7b590289e2f8124f67f2af9b5b
薄熙峰 <727178669@qq.com>
2019-11-03 20:29:13 +0800

    2019年10月22日23:07:47

---
+++ api/src/com/becc/arraylist/DO/Teacher.java
@@ -0,0 +1,11 @@
+package com.becc.arraylist.DO;
+
+public class Teacher extends Employee {
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
