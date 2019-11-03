d2b989ad61b597d99170f2bf14d525657c2488cd
薄熙峰 <727178669@qq.com>
2019-11-03 20:29:27 +0800

    2019年10月22日23:07:47

---
+++ api/src/com/becc/arraylist/DO/Employee.java
@@ -0,0 +1,38 @@
+package com.becc.arraylist.DO;
+
+public class Employee {
+    int age = 200;
+    String name = "员工";
+
+    public Employee() {
+    }
+
+    public Employee(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
