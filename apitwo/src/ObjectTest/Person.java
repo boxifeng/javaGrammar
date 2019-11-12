0337ce8377891d77bf504fe9273f89a4de67f8aa
薄熙峰 <727178669@qq.com>
2019-11-12 22:51:17 +0800

    2019年11月12日22:51:13

---
+++ apitwo/src/ObjectTest/Person.java
@@ -0,0 +1,14 @@
+package ObjectTest;
+
+public class Person {
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ apitwo/src/ObjectTest/Test.java
@@ -0,0 +1,8 @@
+package ObjectTest;
+
+public class Test {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person.toString());
+    }
+}
